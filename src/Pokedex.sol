2f0917415a4ac7aecfd5ebcd453cb89b026ded0e
mouravitor313 <mouravitor0104@gmail.com>
2024-01-11 07:20:44 -0300

    Continuando a criação do Pokedex.sol em cima da base.

--- src/Pokedex.sol
+++ src/Pokedex.sol
@@ -4,87 +4,78 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract PokemonNFT is ERC721 {
+contract Pokedex is ERC721 {
 
-    // Criação da estrutura Pokémon
+    enum typeOf {
+        water,
+        fire,
+        grass
+    }
+
+    enum rarityType {
+        normal,
+        rare,
+        shine
+    }
+
     struct Pokemon {
         string name;
+        typeOf tipo;
+        rarityType rarity;
         uint256 level;
         uint256 hp;
         uint256 attack;
         uint256 defense;
     }
 
-    // Mapeamento de IDs de tokens para Pokémons
     mapping(uint256 => Pokemon) public pokemons;
-
-    // Contador para gerar IDs de tokens únicos
     uint256 public tokenCounter;
-
-    // Endereço do token ERC20 que será usado como pagamento
     address public pokeCoin;
-
-    // Preço em tokens para gerar um Pokémon
     uint256 public price;
 
-    // Evento que é emitido quando um novo Pokémon é criado
     event PokemonCreated(uint256 tokenId, Pokemon pokemon);
 
-    // Construtor que recebe o endereço do token ERC20 e o preço em tokens
-    constructor(address _pokeCoin, uint256 _price) ERC721("PokemonNFT", "PKNFT") {
-        pokeCoin = _pokeCoin;
-        price = _price;
+    constructor() ERC721("Pokedex", "DEX") {
+        pokeCoin = ...; // endereço da pokecoin
+        price = 1;
         tokenCounter = 0;
     }
 
-    // Função que cria um novo Pokémon e o atribui ao remetente
-    // Recebe o endereço do token ERC20, a quantidade de tokens a serem gastos, e os dados do Pokémon
+    function random(uint256 _min, uint256 _max) internal view returns(uint256) {
+        return uint256(keccak256(abi.encodedPacked(blockhash(block.number - 1), msg.sender))) % _max;
+    }
+
+    function generateRarity() internal view returns (rarityType) {
+        uint256 randomNumber = random(100);
+        uint256 normalLimit = 70;
+        uint256 rareLimit = 90;
+
+        if (randomNumber < rareLimit) {
+            return rarityType.normal;
+        } else if (randomNumber < rareLimit) {
+            return rarityType.rare;
+        } else {
+            return rarityType.shine;
+        }
+    }
+
     function createPokemon(
-        address _token,
-        uint256 _amount,
         string memory _name,
-        uint256 _level,
-        uint256 _hp,
-        uint256 _attack,
-        uint256 _defense
+        typeOf _type,
     ) public {
-        // Verifica se o endereço do token é o mesmo que foi definido no construtor
-        require(_token == pokeCoin, "Invalid token address");
+        IERC20 token = IERC20(pokeCoin);
+        require(token.balanceOf(msg.sender) >= price, "Insufficient balance!");
+        token.transferFrom(msg.sender, address(this), price);
+        rarityType _rarity = generateRarity();
 
-        // Verifica se a quantidade de tokens é igual ou maior que o preço
-        require(_amount >= price, "Insufficient tokens");
-
-        // Cria uma interface para o token ERC20
-        IERC20 token = IERC20(_token);
-
-        // Verifica se o remetente tem saldo suficiente de tokens
-        require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
-
-        // Transfere os tokens do remetente para o contrato
-        token.transferFrom(msg.sender, address(this), _amount);
-
-        // Cria um novo Pokémon com os dados fornecidos
         Pokemon memory newPokemon = Pokemon({
             name: _name,
-            level: _level,
-            hp: _hp,
-            attack: _attack,
-            defense: _defense
-        });
+            tipo: _type,
+            rarity: _rarity,
+            level: random(4)
+            hp: random
+        })
 
-        // Gera um novo ID de token
-        uint256 newTokenId = tokenCounter;
-
-        // Incrementa o contador de tokens
-        tokenCounter++;
-
-        // Cria um novo token NFT e o atribui ao remetente
-        _safeMint(msg.sender, newTokenId);
-
-        // Associa o token ao Pokémon
-        pokemons[newTokenId] = newPokemon;
-
-        // Emite o evento de criação do Pokémon
-        emit PokemonCreated(newTokenId, newPokemon);
     }
+
 }
