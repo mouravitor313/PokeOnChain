9ca69dde879d90dfc9ab32acc38553740496782b
mouravitor313 <mouravitor0104@gmail.com>
2024-01-12 15:19:13 -0300

    Corrigi o erro na função random do contrato Pokedex.sol. Era um erro de logica e sintaxe na função mod(substituida pelo tryMod). Acabei removendo o contrato Battles.sol para podermos reformular a funcionalidade de batalhas.

--- src/Pokedex.sol
+++ src/Pokedex.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 
 contract Pokedex is ERC721 {
 
@@ -35,24 +37,26 @@ contract Pokedex is ERC721 {
 
     event PokemonCreated(uint256 tokenId, Pokemon pokemon);
 
-    constructor() ERC721("Pokedex", "DEX") {
-        pokeCoin = ...; // endereço da pokecoin, tem que deployar...
+    constructor(address Pokecoin) ERC721("Pokedex", "DEX") {
+        pokeCoin = Pokecoin; // endereço da pokecoin, tem que deployar...
         price = 1;
         tokenCounter = 0;
     }
 
+    using Math for uint256;
+
     function random(
-        uint256 _min,
-        uint256 _max
-        ) internal view returns(uint256) {
-        using Math for uint256;
-        uint256 diff = _max - _min;
-        bytes32 hash = keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender));
-        uint256 number = uint256(hash);
-        uint256 remainder = number.mod(diff);
-        uint256 result = remainder + _min;
-        return result;
-    }
+    uint256 _min,
+    uint256 _max
+    ) internal view returns(uint256) {
+    uint256 diff = _max - _min;
+    bytes32 hash = keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender));
+    uint256 number = uint256(hash);
+    (bool success, uint256 remainder) = Math.tryMod(number, diff); // Use a função tryMod
+    require(success, "Division for 0 or overflow"); // Verifique se a operação foi bem-sucedida
+    uint256 result = remainder + _min;
+    return result;
+  }
 
     function generateRarity() internal view returns (rarityType) {
         uint256 randomNumber = random(0, 100);
@@ -70,7 +74,7 @@ contract Pokedex is ERC721 {
 
     function createPokemon(
         string memory _name,
-        typeOf _type,
+        typeOf _type
     ) public {
         IERC20 token = IERC20(pokeCoin);
         require(token.balanceOf(msg.sender) >= price, "Insufficient balance!");
